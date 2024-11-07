13bb93237058558dadbd5a9d0d0a0118d0671f59
parangstar <165374472+parangstar@users.noreply.github.com>
2024-11-07 15:52:15 +0900

    [Chore] #5 경로수정

--- rookiefit/src/App.tsx
+++ rookiefit/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import Header from './layout/header/header';
 import CalenderPage from './calendar/calenderPage';
 import LoginPage from './logIn/LoginPage';
-import SignUpPage from './signup/signuppage';
+import SignUpPage from './signup/signUpPage';
 
 
 const App: React.FC = () => {
@@ -12,13 +12,13 @@ const App: React.FC = () => {
       <div>
         <Header />
         <Routes>
-          <Route path="/login" element={<></>} />
-          <Route path="/signup" element={<></>} />
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/signup" element={<SignUpPage/>} />
           <Route path="/mypage" element={<></>} />
           <Route path="/calendar" element={<CalenderPage />} />
           <Route path="/diet" element={<></>} />
-          <Route path="/community" element={<LoginPage />}/>
-          <Route path="/signUpPage" element={<SignUpPage/>} />
+          <Route path="/community" element={<></>} />
+          <Route path="/signUpPage" element={<></>} />
         </Routes>
       </div>
     </BrowserRouter>
