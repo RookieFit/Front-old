360d47cc8067a16efb51c98b31919205ff0d8a36
parangstar <165374472+parangstar@users.noreply.github.com>
2024-11-07 11:47:15 +0900

    [Feat] #5 완성본???

--- rookiefit/src/App.tsx
+++ rookiefit/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Header from './layout/header/header';
 import CalenderPage from './calendar/calenderPage';
+import LoginPage from './logIn/LoginPage';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
+        <LoginPage />
         <Routes>
           <Route path="/login" element={<></>} />
           <Route path="/signup" element={<></>} />
