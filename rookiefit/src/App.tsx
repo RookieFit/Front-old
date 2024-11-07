a253aa7a332eb87523fb85232e59792e2abd020d
parangstar <165374472+parangstar@users.noreply.github.com>
2024-11-07 11:47:15 +0900

    [Feat] #5 완성본???

--- rookiefit/src/App.tsx
+++ rookiefit/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Header from './layout/header/header';
+import LoginPage from './logIn/LoginPage';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
+        <LoginPage />
         <Routes>
           <Route path="/mypage" element={<></>} />
           <Route path="/calendar" element={<></>} />
--- rookiefit/src/logIn/LoginPage.tsx
+++ rookiefit/src/logIn/LoginPage.tsx
@@ -1,67 +1,75 @@
-import React, { useState } from 'react';
-import './LoginPage.css';
+import './loginPage.css';
 
 function LoginPage() {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
+    /*  로그인 */
+    const idClickHandler = () => {
+        alert("click");
+    }
+     /*  아이디/비번 찾기 */
+    const findClickHandler = () => {
+        alert("find id and password");
+    }
+    /* 간편 로그인 클릭 */
+    const naverClickHandler = () => {
+        alert("naver login~~");
+    }
+    const kakaoClickHandler = () => {
+        alert("kakao login~~");
+    }
+    const googleClickHandler = () => {
+        alert("google login~~");
+    }
 
-    // ID와 Password 변경 핸들러 함수
-    const handleIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setId(event.target.value);
-    };
-
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
+     /*  회원가입 */
+     const signupClickHandler = () => {
+        alert("회원가입~~");
+    }
 
     return (
         <div id="sign-in-wrapper">
-            <div className="login">로그인</div>
-            <div className="login_email_id_box">
+            <div className='login_title'>로그인</div>
+
+            {/* ID Input Field */}
+            <div >
                 <input
+                    className='id_password_box'
                     type="text"
                     placeholder="이메일 아이디"
-                    value={id}
-                    onChange={handleIdChange}
-                    className="email_id_letter"
+                
                 />
             </div>
 
             {/* Password Input Field */}
-            <div className="Rectangle_22">
+            <div >
                 <input
+                    className='id_password_box'
                     type="password"
                     placeholder="Password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    className="password_letter"
+                    
+                    
                 />
             </div>
 
             {/* Login Button */}
-            <div className="Rectangle_21">
-                <button className="login_letter">로그인</button>
+            <div >
+                <button className=' login_button -right,' onClick={idClickHandler}>로그인</button>
             </div>
+            {/* Forgot ID / Password */}
+            <button className='find_id_password_button' onClick={findClickHandler}>아이디 찾기 | 비밀번호 찾기</button>
+
+            <div className='underline'></div>
 
             {/* Sign Up Button */}
-            <div className="Rectangle_23">
-                <button className="sign_up_letter">회원가입</button>
+            <div>
+                <button className='sign_in_button' onClick={signupClickHandler}>회원가입</button>
             </div>
-
-            {/* Forgot ID / Password */}
-            <div className="find_id password_letter">아이디 찾기 | 비밀번호 찾기</div>
 
             {/* Social Login Section */}
-            <div className="easy_login">간편 로그인</div>
-            <div className="seperate_line_bottom"></div>
-            <div className="naver_circle">
-                <div className="naver_letter">네이버</div>
-            </div>
-            <div className="kakao_circle">
-                <div className="kakao_letter">카카오</div>
-            </div>
-            <div className="google_circle">
-                <div className="googl_letter">구글</div>
+            <div className='easy_login_text'>간편 로그인</div>
+            <div className='sns_login_container'>
+                <button className='naver_login' onClick={naverClickHandler}></button>
+                <button className='kakao_login' onClick={kakaoClickHandler}></button>
+                <button className='google_login'onClick={googleClickHandler}></button>
             </div>
         </div>
     );
