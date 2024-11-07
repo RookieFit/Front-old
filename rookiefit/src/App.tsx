d549e184f7f020b87d96cb88905df94618709584
parangstar <165374472+parangstar@users.noreply.github.com>
2024-11-07 15:34:09 +0900

    [Feat] #5 로그인+회원가입 조금

--- rookiefit/src/App.tsx
+++ rookiefit/src/App.tsx
@@ -2,18 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Header from './layout/header/header';
 import LoginPage from './logIn/LoginPage';
+import SignUpPage from './signup/signuppage';
+
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <LoginPage />
         <Routes>
           <Route path="/mypage" element={<></>} />
           <Route path="/calendar" element={<></>} />
           <Route path="/diet" element={<></>} />
-          <Route path="/community" element={<></>} />
+          <Route path="/community" element={<LoginPage />}/>
+          <Route path="/signUpPage" element={<SignUpPage/>} />
         </Routes>
       </div>
     </BrowserRouter>
