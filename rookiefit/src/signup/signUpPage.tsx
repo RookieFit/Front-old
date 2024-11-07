125d911e20517fee0bb49828e8c396268f73175f
parangstar <165374472+parangstar@users.noreply.github.com>
2024-11-07 15:47:41 +0900

    [Chore] #5 save

--- rookiefit/src/signup/signUpPage.tsx
+++ rookiefit/src/signup/signUpPage.tsx
@@ -1,7 +1,7 @@
 import './SignUpPage.css';
 
 function SignUpPage() {
-    
+
     return (
         <div id="sign-up-wrapper">
             <div className='signup_title'>회원 가입</div>
@@ -12,6 +12,39 @@ function SignUpPage() {
                     type="text"
                     placeholder="이름을 입력해주세요">
                 </input>
+            </div>
+
+            <div className='name_container'>
+                <div>아이디</div>
+                <input
+                    type="text"
+                    placeholder="아이디를 입력해주세요">
+                </input>
+            </div>
+
+            <div className='name_container'>
+                <div>패스워드</div>
+                <input
+                    type="text"
+                    placeholder="패스워드를 입력해주세요">
+                </input>
+            </div>
+
+            <div className='name_container'>
+                <div>휴대전화</div>
+                <input
+                    type="text"
+                    placeholder="전화번호를 입력해주세요">
+                </input>
+            </div>
+
+            <div className='name_container'>
+                <div>인증번호</div>
+                <input
+                    type="text"
+                    placeholder="인증번호를 입력해주세요">
+                </input>
+
             </div>
         </div>
     );
