d96688fc7752d76b3e74911a7e2b16757658039b
동영 <57888678+dahli4@users.noreply.github.com>
2024-11-29 11:31:43 +0900

    [Feat] #102 식단 api 추가

---
+++ rookiefit/src/apis/request/diet/inputFoodInfoRequest.dto.ts
@@ -0,0 +1,7 @@
+export default interface InputFoodInfoRequestDto {
+    food_name: string
+    chocdf: number
+    prot: number
+    fatce: number
+    enerc: number
+}
---
+++ rookiefit/src/apis/response/diet/index.ts
@@ -0,0 +1,11 @@
+import DeleteUserDietListResponseDto from "./deleteUserDietListResponse.dto"
+import GetDietDataResponseDto from "./getDietDataResponse.dto"
+import GetDietDataDetailResponseDto from "./getDietDataDetailResponse.dto"
+import InputFoodInfoResponseDto from "./inputFoodInfoResponse.dto"
+import InputUserDietListResponseDto from "./inputUserDietListResponse.dto"
+
+export type {
+    DeleteUserDietListResponseDto, GetDietDataResponseDto,
+    GetDietDataDetailResponseDto,
+    InputFoodInfoResponseDto, InputUserDietListResponseDto,
+}
