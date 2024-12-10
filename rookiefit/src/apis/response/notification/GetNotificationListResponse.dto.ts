69c00861139eafe4a9f071f628bcc3916ccc5dea
21270003 <rudddms1533@naver.com>
2024-12-10 11:30:30 +0900

    [feat]api..dto..

---
+++ rookiefit/src/apis/response/notification/GetNotificationListResponse.dto.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import ResponseDto from "../response.dto";
+
+export default interface GetNotificationListResponseDto extends ResponseDto {
+    notificationId: string;
+    notificationAuthor: string;
+    notificationContent: string;
+    notificationTitle: string;
+    notificationCreatedTime: string;
+    notificationIsModified: Boolean;
+    notificationImageUri: string;
+    notificationBoardDown: Boolean;
+    notificationBoardUpdatedTime: string;
+}
---
+++ rookiefit/src/apis/response/trainer/index.ts
@@ -0,0 +1,6 @@
+import InputTrainerResponseDto from "./InputTrainerResponse.dto"
+import LicenseApproveResponseDto from "./LicenseApproveResponse.dto"
+
+export type {
+    LicenseApproveResponseDto, InputTrainerResponseDto,
+}
